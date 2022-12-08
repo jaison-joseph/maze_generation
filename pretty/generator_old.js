62bbf8e3ebc0c8efe01de30aaf2695cb4c7f1ec2
jaison_windows <jaisonj2001@gmail.com>
2022-12-08 12:42:54 -0500

    more stable js solver, images, demo maze files

--- pretty/generator_old.js
+++ pretty/generator_old.js
@@ -1,4 +1,3 @@
-
 var solution = [];
 var checkpointSolution = [];
 var c = null;
@@ -10,18 +9,12 @@ function traceBack_3(lk, fitness, start, end) {
     const size_ = 30;
     let nextSteps = [];
     let stacks = [[start]];
-    let debug = false;
-    if (start[0] == 6 && start[1] == 12 && end[0] == 29 && end[1] == 29) {
-        debug = true;
-    }
-    if  (debug) {
-        console.log("initial stacks: ", stacks)
-        console.log("fitness: ", fitness)
-        console.log("lk: ", lk)
-    }
+    console.log("initial stacks: ", stacks)
+    console.log("fitness: ", fitness)
+    console.log("lk: ", lk)
     
     let newStacks = [];
-    console.log("debug: ", debug ? "true" : "false");
+    // console.log("[0]: ", stacks[0]);
 
     // stacks.forEach(s => {console.log("stack: ", s);});
 
@@ -33,9 +26,7 @@ function traceBack_3(lk, fitness, start, end) {
         for (let i = 0 ; i < stacks.length ; i++) {
         // stacks.forEach(stack => {
             stack = stacks[i];
-            if (debug) {
-                console.log("stack: ", stack);
-            }
+            // console.log("stack: ", stack);
             pt = stack[stack.length-1];
             dist = lk[pt[0]][pt[1]];
             dist += 1;
@@ -66,73 +57,6 @@ function traceBack_3(lk, fitness, start, end) {
                 stack.pop();
             });
         };
-        stacks = newStacks;
-        if (stacks.length == 0) {
-            return;
-        }
-        if (debug) {
-            // console.log("newStacks: ", newStacks);
-            console.log("len: ", stacks[0].length);
-        }
-    }
-    // return the path on the stacks with last element == end square
-    return stacks.filter(x => x[x.length-1][0] == end[0] && x[x.length-1][1] == end[1])[0];
-}
-
-// instead of start -> end, do end -> start
-function traceBack_4(lk, fitness, start, end) {
-    const size_ = 30;
-    let nextSteps = [];
-    let stacks = [[end]];
-    console.log("initial stacks: ", stacks);
-    console.log("fitness: ", fitness);
-    console.log("lk: ", lk);
-    
-    let newStacks = [];
-    // console.log("[0]: ", stacks[0]);
-
-    // stacks.forEach(s => {console.log("stack: ", s);});
-
-    // while(Math.max(stacks.map(x=>x.length)) != fitness) {
-    for (let i = 0 ; i <= fitness ; i++) {
-        newStacks = [];
-        // if (stacks[0].length >= fitness) {
-        //     break;
-        // }
-        // for (let i = 0 ; i < stacks.length ; i++) {
-        stacks.forEach(stack => {
-            // stack = stacks[i];
-            // console.log("stack: ", stack);
-            pt = stack[stack.length-1];
-            dist = lk[pt[0]][pt[1]];
-            dist -= 1;
-            nextSteps = [];
-    
-            foo = [pt[0]+1, pt[1]];
-            if (foo[0] < size_ && lk[foo[0]][foo[1]] === dist) {
-                nextSteps.push(foo);
-            }
-        
-            foo = [pt[0], pt[1]+1];
-            if (foo[1] < size_ && lk[foo[0]][foo[1]] === dist) {
-                nextSteps.push(foo);
-            }
-            foo = [pt[0]-1, pt[1]];
-            if (foo[0] >= 0 && lk[foo[0]][foo[1]] === dist) {
-                nextSteps.push(foo);
-            }
-        
-            foo = [pt[0], pt[1]-1];
-            if (foo[1] >= 0 && lk[foo[0]][foo[1]] === dist) {
-                nextSteps.push(foo);
-            }
-
-            nextSteps.forEach(s => {
-                stack.push(s);
-                newStacks.push([...stack]);
-                stack.pop();
-            });
-        });
         // console.log("newStacks: ", newStacks);
         stacks = newStacks;
         // console.log("len: ", stacks[0].length);
@@ -141,47 +65,7 @@ function traceBack_4(lk, fitness, start, end) {
     //     console.log("stack: ", s);
     // });
     // return the path on the stacks with last element == end square
-    return stacks.filter(x => x[x.length-1][0] == start[0] && x[x.length-1][1] == start[1])[0];
-}
-
-function splitJobs(lk, fitness, start, end) {
-    console.log(`entering split for ${start}->${end} | fitness: ${fitness}`);
-    console.log(lk);
-    var midPtDist = Math.floor(fitness/2);
-    var pt1 = null;
-    var pt2 = null;
-    var gotcha = false;
-    for (let i = 0 ; i < 30 ; i++) {
-        for (let j = 0 ; j < 30 ; j++) {
-            if (lk[i][j] == midPtDist) {
-                console.log(`found: ${i}, ${j}`);
-                console.log(`PART 1: calling traceBack_3 on: ${midPtDist+1}, ${start}, ${[i, j]}`);
-                pt1 = traceBack_3(lk, midPtDist+1, start, [i, j]);
-                if (pt1 === undefined)
-                    continue;
-                if (fitness%2 == 0) {
-                    console.log(`PART 2: calling traceBack_3 on: ${midPtDist+1}, ${[i, j]}, ${end}`);
-                    pt2 = traceBack_3(lk, midPtDist+1, [i, j], end);
-                }
-                else {
-                    console.log(`PART 2: calling traceBack_3 on: ${midPtDist+2}, ${[i, j]}, ${end}`);
-                    pt2 = traceBack_3(lk, midPtDist+2, [i, j], end);
-                }
-                if (pt2 === undefined)
-                    continue;
-                gotcha = true;
-                break
-            }
-            if (gotcha)
-                break;
-        }
-        if (gotcha)
-            break;
-    }
-    if (pt1 == null || pt2 == null)
-        return ;
-    return pt1.concat(pt2);
-    
+    return stacks.filter(x => x[x.length-1][0] == end[0] && x[x.length-1][1] == end[1])[0];
 }
 
 // returns path length, and array of steps from start to end through 'maze'
@@ -213,9 +97,6 @@ function pathFinder(start, end) {
             if (foo[0] != end[0] || foo[1] != end[1]) {
                 q.push(foo);
             }
-            else {
-                break;
-            }
         }
 
         foo = [pt[0], pt[1]+1];
@@ -223,9 +104,6 @@ function pathFinder(start, end) {
             lk [foo[0]] [foo[1]] = dist;
             if (foo[0] != end[0] || foo[1] != end[1]) {
                 q.push(foo);
-            }
-            else {
-                break;
             }
         }
 
@@ -235,9 +113,6 @@ function pathFinder(start, end) {
             if (foo[0] != end[0] || foo[1] != end[1]) {
                 q.push(foo);
             }
-            else {
-                break;
-            }
         }
 
         foo = [pt[0], pt[1]-1];
@@ -246,26 +121,14 @@ function pathFinder(start, end) {
             if (foo[0] != end[0] || foo[1] != end[1]) {
                 q.push(foo);
             }
-            else {
-                break;
-            }
         }
     }
 
-    const fitness = lk[end[0]][end[1]];
-    let traceBackResult = null;
-    if (fitness > 170) {
-        traceBackResult = splitJobs(lk, fitness, start, end);
-    }
-    else {
-        traceBackResult = traceBack_3(lk, lk[end[0]][end[1]]+1, start, end);  
-    }
-
+    const traceBackResult = traceBack_3(lk, lk[end[0]][end[1]]+1, start, end);  
 
     console.log("traceBack: ", traceBackResult);
 
     return [traceBackResult, lk[end[0]][end[1]]];
-    return lk;
 }
   
 
@@ -304,63 +167,12 @@ function process(input) {
     );
 
     console.log("checkpointSolution: ", checkpointSolution);
-    
+
     console.log("path from pathFinder([0, 0], checkpoints[0]): ", longSolution[0]);
     console.log("path from pathFinder(checkpoints[0], checkpoints[1]): ", longSolution[0]);
     console.log("path from pathFinder(checkpoints[1], checkpoints[2]): ", longSolution[1]);
     console.log("path from pathFinder(checkpoints[2], checkpoints[3]): ", longSolution[2]);
     console.log("path from pathFinder(checkpoints[3], [29, 29]): ", longSolution[3]);
-
-    for (let [i, row] of matrix.entries()) {
-        for (let [j, val] of row.entries()) {
-            if (val === 1) {
-                // Draw a black square
-                ctx.fillStyle = "black";
-                ctx.fillRect(i*30, j*30, 30, 30);
-            } else {
-                // Draw a white square
-                ctx.fillStyle = "white";
-                ctx.fillRect(i*30, j*30, 30, 30);
-            }
-        }  
-    }
-}
-
-function debugProcess(input) {
-    matrix = [];
-    input.split("\n").forEach(l => {
-        l = l.trim();
-        l = l.split(", ");
-        l[l.length- 1] = l[l.length- 1][0];
-        matrix.push(l.map(x=>Number(x)));
-    });
-    console.log("matrix", matrix);
-    let soln = pathFinder([0, 0], [29, 29]);
-    solution = soln[0];
-    console.log("from start to end: ", soln);
-
-    let longSolution = [
-        pathFinder([0, 0], checkpoints[0]),
-        pathFinder(checkpoints[0], checkpoints[1]),
-        pathFinder(checkpoints[1], checkpoints[2]),
-        pathFinder(checkpoints[2], checkpoints[3]),
-        pathFinder(checkpoints[3], [29, 29]),
-    ];
-
-    checkpointSolution = longSolution[0][0].concat(
-        longSolution[1][0].slice(1),
-        longSolution[2][0].slice(1),
-        longSolution[3][0].slice(1),
-        longSolution[4][0].slice(1),
-    );
-
-    console.log("checkpointSolution: ", checkpointSolution);
-
-    console.log("path from pathFinder([0, 0], checkpoints[0]): ", longSolution[0]);
-    console.log("path from pathFinder(checkpoints[0], checkpoints[1]): ", longSolution[1]);
-    console.log("path from pathFinder(checkpoints[1], checkpoints[2]): ", longSolution[2]);
-    console.log("path from pathFinder(checkpoints[2], checkpoints[3]): ", longSolution[3]);
-    console.log("path from pathFinder(checkpoints[3], [29, 29]): ", longSolution[4]);
 
     for (let [i, row] of matrix.entries()) {
         for (let [j, val] of row.entries()) {
@@ -429,7 +241,7 @@ function previewFile() {
     reader.addEventListener("load", () => {
       // this will then display a text file
     //   content.innerText = reader.result;
-      debugProcess(reader.result);
+      process(reader.result);
     }, false);
   
     if (file) {
