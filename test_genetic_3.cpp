7812e453a381992b902ba954b7d0a5e3ce3f45d6
jaison-joseph <jaisonj2001@gmail.com>
2024-04-23 09:14:08 -0400

    cleanup project directory and genetic_3

---
+++ test_genetic_3.cpp
@@ -0,0 +1,99 @@
+#include "genetic_3.cpp"
+
+void testUniformMutation() {
+    array<array<bool, size_>, size_> m;
+    for (auto&i : m) {
+        i.fill(false);
+    }
+    cout << "before mutation: \n" << m;
+    uniformMutation(m);
+    cout << "after mutation: \n" << m;
+}
+
+
+void testUniformCrossover() {
+    array<array<bool, size_>, size_> m1, m2;
+    for (auto&i : m1) 
+        i.fill(false);
+    for (auto&i : m2) 
+        i.fill(true);
+    cout << "before mutation: \n" << m1 << "\n " << m2;
+    uniformCrossover(m1, m2);
+    cout << "before mutation: \n" << m1 << "\n " << m2;
+}
+
+
+void testrandomNumberGenerator() {
+    random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<mt19937::result_type> getNum(1,1000);
+    cout << "printing 100 numbers in the range[1, 100]: ";
+    for (int i = 0 ; i < 100 ; ++i) {
+        cout << float(getNum(rng))/1000.0f << " ," ;
+    }
+}
+
+void testFitness_2() {
+    // converted later into a 2d bool array; 
+    // the purpose of this is to design the maze to be tested, 
+    // the 5's are the checkpoint locations
+    array<array<int, size_>, size_> designer = {
+        array<int, size_> {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 5, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        array<int, size_> {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},   
+        array<int, size_> {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},   
+        array<int, size_> {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},   
+        array<int, size_> {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},   
+        array<int, size_> {1, 1, 1, 1, 1, 1, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},   
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,},   
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,},   
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,},   
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,},   
+        array<int, size_> {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,}
+    };
+
+    array<array<bool, size_>, size_> maze;
+    for (int i = 0 ; i < size_ ; ++i)
+        for (int j = 0 ; j < size_ ; ++j)
+            maze[i][j] = (designer[i][j] == 1);
+
+    cout << "\n pathfinder result: " << pathFinder(maze, entrance_, exit_);
+    cout << "\n fitness result: " << fitness_3(maze);
+}
+
+void testShuffle() {
+    array<array<int, 2>, 4> c = checkpoints_;
+    int ct = 0;
+    do {
+        cout << ++ct << ". ";
+        for (array<int, 2>& i : c) {
+            cout << i << ", ";
+        }
+        // cout << c[0] << ", " << c[1] << ", " << c[2] << ", " << c[3] << '\n';
+        cout << '\n';
+    } while (next_permutation(c.begin(), c.end()));
+}
+
+void testPathFinder() {
+    auto m = genMaze();
+    auto result = pathFinder(m, entrance_, exit_);
+    cout << "\n path dist: " << result;
+}
