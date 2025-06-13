e2e1a9140d0a4e00c6fee6e7c49fbd6fc3f98a28
jaison-joseph <jaisonj2001@gmail.com>
2025-06-13 18:22:13 -0400

    task q comments

--- play/task_queue.cpp
+++ play/task_queue.cpp
@@ -5,12 +5,40 @@
 #include <chrono>
 #include <atomic>
 
+/**
+ * 
+ * Simple queue for 1 producer, multiple consumer
+ * 
+ * methods:
+ * 
+ * TaskQueueClient()
+ * 		- constructor
+ * 
+ * void add_task(uint8_t x)
+ * 		- non blocking
+ * 		- add a task to task queue
+ * 		- x: input to queue
+ * 
+ * void terminate_threads()
+ * 		- stops task processing
+ * 		- calling any method on this object after calling
+ * 		terminate_threads is UB
+ * 
+ * int32_t get_task()
+ * 		- blocking method for consumers
+ * 		- if return value is -1, then terminate_threads was called
+ * 		- else, the return value is the task value
+*/
 #pragma once
 class TaskQueueClient {
 	public:
+		// actual store of tasks, we ensure atomic access to the store among producer/consumers
 		static std::vector<uint8_t> tasks;
+		// for synchronization, we construct unique_lock w/ this mutex
 		static std::mutex mutex;
+		// for synchronization, used for avoiding busy waiting
 		static std::condition_variable cv;
+		// we use 'inline' since we can give it initial value in declaration itself
 		inline static bool termination_flag = false;
 
 		TaskQueueClient() {
@@ -30,15 +58,19 @@ class TaskQueueClient {
 			cv.notify_all();
 		}
 		
-		// if we return -1, then the thread was terminated
-		// else, a whole number will be returned (uint8_t)
-		// this method is blocking
 		int32_t get_task() { 
 			// as soon as lock goes out of scope, lock is released -> mutex is released
 			std::unique_lock<std::mutex> lock(mutex);
 			if (tasks.size() == 0) {
 				// this wait releases the hold on lock -> mutex is released
-				cv.wait(lock, []{ return tasks.size() > 0 || termination_flag; });
+				cv.wait(lock, []{ 
+					// this condition is the second check to ensure
+					// that we don't get hold of lock unless 
+					// 1. tasks queue is non-empty
+					// 		OR
+					// 2. termination flag is set
+					return tasks.size() > 0 || termination_flag; 
+				});
 			}
 			if (termination_flag) return -1;
 			int32_t task = tasks.back();
